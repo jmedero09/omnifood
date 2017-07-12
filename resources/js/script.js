35d1d3561c27585a6304ff1061f0c67b29baa159
Jesus Medero <jmedero09@gmail.com>
2017-07-12 01:18:05 -0400

    added animation

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,18 +1,5 @@
 $(document).ready(function(){
-
-//     $('h1').click(function(){
-//         $(this).css('background-color','#ff0000');
-//     });
-
-// });
-
-// var waypoint = new Waypoint({
-//   element: document.getElementById('px-offset-waypoint'),
-//   handler: function(direction) {
-//     notify('I am 20px from the top of the window')
-//   },
-//   offset: 20 
-
+//This is for the sticky naviation 
     $('.js--section-features').waypoint(function(direction){
         if(direction=='down'){
             $('nav').addClass('sticky');
@@ -22,7 +9,74 @@ $(document).ready(function(){
     },{
         offset:'60px;'
     });
+//scroll on buttons
+    $('.js--scroll-to-plans').click(function(){
+        $('html, body').animate({scrollTop:$('.js--section-plans').offset().top},1000);
+    });
+    $('.js--scroll-to-start').click(function(){
+        $('html, body').animate({scrollTop:$('.js--section-features').offset().top},1000);
+    });    
 
+//navogation scroll
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 
+  //Animations on scroll
+  $('.js--wp-1').waypoint(function(){
+    $('.js--wp-1').addClass('animated fadeIn');
+  },{
+    offset:'50%'
+  });
 
- })
+  $('.js--wp-2').waypoint(function(){
+    $('.js--wp-2').addClass('animated fadeIn');
+  },{
+    offset:'50%'
+  });  
+
+  $('.js--wp-3').waypoint(function(){
+    $('.js--wp-3').addClass('animated fadeInUp');
+  },{
+    offset:'50%'
+  });
+
+  $('.js--wp-4').waypoint(function(){
+    $('.js--wp-4').addClass('animated pulse');
+  },{
+    offset:'50%'
+  });    
+})
